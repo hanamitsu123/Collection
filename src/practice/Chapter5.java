f7c2722231e04a3a86cb9ebec997fc14ec1c5ceb
MitsumasaTokunaga <163833605+hanamitsu123@users.noreply.github.com>
2024-04-07 23:11:46 +0900

    課題再提出

---
+++ src/practice/Chapter5.java
@@ -0,0 +1,23 @@
+package practice;
+
+import java.time.LocalDate;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Chapter5{
+	public static void main(String[] args) {
+		
+		// TreeMapを使用してタスクを生成
+		TreeMap<LocalDate, String> tasks = new TreeMap<>();
+		tasks.put(LocalDate.of(2021, 10, 21), "牛乳を買う");
+		tasks.put(LocalDate.of(2021, 9, 15), "○○社面談");
+		tasks.put(LocalDate.of(2021, 12, 4), "手帳を買う");
+		tasks.put(LocalDate.of(2021, 8, 10), "散髪に行く");
+		tasks.put(LocalDate.of(2021, 11, 9), "スクールの課題を解く");
+
+		// タスクをリストとして表示
+		for (Map.Entry<LocalDate, String> entry : tasks.entrySet()) {
+			System.out.println(entry.getKey() + ":" + entry.getValue());
+		}
+	}	
+}
--- src/practice/Task.java
+++ src/practice/Task.java
@@ -1,22 +1,21 @@
 package practice;
 
 import java.time.LocalDate;
-import java.util.Map;
-import java.util.TreeMap;
 
-public class Task {
-    public static void main(String[] args) {
-        // TreeMapを使用してタスクを生成
-        TreeMap<LocalDate, String> tasks = new TreeMap<>();
-        tasks.put(LocalDate.of(2021, 10, 21), "牛乳を買う");
-        tasks.put(LocalDate.of(2021, 9, 15), "○○社面談");
-        tasks.put(LocalDate.of(2021, 12, 4), "手帳を買う");
-        tasks.put(LocalDate.of(2021, 8, 10), "散髪に行く");
-        tasks.put(LocalDate.of(2021, 11, 9), "スクールの課題を解く");
-        
-        // タスクをリストとして表示
-        for (Map.Entry<LocalDate, String> entry : tasks.entrySet()) {
-            System.out.println(entry.getKey() + ":" + entry.getValue());
-        }
-    }
+public class Task { 
+		
+	private LocalDate date;
+	private String content;
+
+	public Task(LocalDate date, String content) {
+		this.date = date;
+		this.content = content;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+	public String getContent() {
+		return content;
+	}
 }
